42ac81ae934bfd218dc18434ebe7305dc13d3083
Michael Vo <michaelmvo91@gmail.com>
2019-11-15 17:18:03 -1000

    Using ReactRouter

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
 import './scss/App.scss'
+import Navigation from "./components/Navigation";
 
 function App() {
-  return (
-    <div>
-        Hello World
-    </div>
-  );
+    return (
+        <Router>
+            <Navigation/>
+        </Router>
+    );
 }
 
 export default App;
